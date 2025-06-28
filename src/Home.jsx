73d13f4149fe2da6c4a1f2bd511c2a0ed8b11b8f
Prashanth <mudunuriprashanth143@gmail.com>
2025-06-28 23:33:13 +0530

    Updated Home.jsx: added image and bio container

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import { Navigate, useNavigate } from 'react-router-dom'
-import './CSS/Home.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './CSS/Home.css';
+import p1 from './images/prashanth.jpg';
+
 function Home() {
-    const navigate = useNavigate();
-    const handleredirect=()=>{
-      navigate('/signup');
-    }
+  const navigate = useNavigate();
+
+  const handleredirect = () => {
+    navigate('/signup');
+  };
+
   return (
     <div>
-       <button id ="bt1" onClick={handleredirect}>Welcome to the website</button>
+      <button id="bt1" onClick={handleredirect}>
+        Welcome to the website
+      </button>
+
+      <div className="creator-container">
+        <div className="left-img">
+          <img src={p1} alt="Prashanth" />
+        </div>
+        <div className="right-text">
+          <h2>Mudunuri Prashanth</h2>
+          <p>Full Stack Web Developer (MERN)</p>
+          <h3>Researchers</h3>
+          <ul>
+            <li>Rasheed</li>
+            <li>Jafar</li>
+          </ul>
+          <p><i>(They helped in gathering college & CG details)</i></p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
