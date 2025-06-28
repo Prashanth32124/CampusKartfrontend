be21fece6e0bbc9aabd89b02a251edc646fdfe24
Prashanth <mudunuriprashanth143@gmail.com>
2025-06-28 23:56:18 +0530

    Updated Home section layout and styles for creator and researchers

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './CSS/Home.css';
 import p1 from './images/prashanth.png';
+import p2 from './images/rasheed.png';
+import p3 from './images/jafar.png';
 
 function Home() {
   const navigate = useNavigate();
@@ -16,6 +18,7 @@ function Home() {
         Welcome to the website
       </button>
 
+      {/* Creator Section */}
       <div className="creator-container">
         <div className="left-img">
           <img src={p1} alt="Prashanth" />
@@ -23,12 +26,30 @@ function Home() {
         <div className="right-text">
           <h2>Mudunuri Prashanth</h2>
           <p>Full Stack Web Developer (MERN)</p>
-          <h3>Researchers</h3>
-          <ul>
-            <li>Rasheed</li>
-            <li>Jafar</li>
-          </ul>
-          <p><i>(They helped in gathering college & CG details)</i></p>
+        </div>
+      </div>
+
+      {/* Researchers Section */}
+      <div className="researchers-heading">
+        <h3>Researchers</h3>
+        <p><i>(They helped in gathering college & CG details)</i></p>
+      </div>
+
+      <div className="researchers">
+        <div className="researcher-card">
+          <img src={p2} alt="Rasheed" />
+          <div className="info">
+            <h4>Rasheed</h4>
+            <p>Helped collect CG & college data</p>
+          </div>
+        </div>
+
+        <div className="researcher-card">
+          <img src={p3} alt="Jafar" />
+          <div className="info">
+            <h4>Jafar</h4>
+            <p>Helped collect CG & college data</p>
+          </div>
         </div>
       </div>
     </div>
