9c0aa8785c5a4688243aa2f73fd758cc0a9080a1
Prashanth <mudunuriprashanth143@gmail.com>
2025-08-12 16:20:50 +0530

    Added data  and logic

--- src/Unauthorized.jsx
+++ src/Unauthorized.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Unauthorized() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/login');  
+    }, 3000);
+
+    return () => clearTimeout(timer); 
+  }, [navigate]);
+
   return (
     <div>
-      <h1>access deniend ,please login first</h1>
+      <h1>Access denied, please login first</h1>
     </div>
-  )
+  );
 }
 
-export default Unauthorized
+export default Unauthorized;
